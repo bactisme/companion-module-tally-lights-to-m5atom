907afa6d24168f967ff162e405795fdf305496fd
Baptiste Michaud <baptiste@humanoid.fr>
2023-11-25 19:29:47 +0100

    feat brightness

--- main.js
+++ main.js
@@ -2,6 +2,9 @@ const { InstanceBase, Regex, runEntrypoint, InstanceStatus } = require('@compani
 const UpgradeScripts = require('./upgrades')
 const UpdateVariableDefinitions = require('./variables')
 
+const http = require('node:http');
+var querystring = require('querystring');
+
 const MAX_CAM = 3;
 
 class ModuleInstance extends InstanceBase {
@@ -10,13 +13,17 @@ class ModuleInstance extends InstanceBase {
 	}
 
 	async init(config) {
-		this.config = config
+		this.config = config;
 
-		this.updateStatus(InstanceStatus.Ok)
+        this.current_color = null;
+        this.current_brightness = null;
 
-		this.updateActions() // export actions
-		this.updateVariableDefinitions() // export variable definitions
+		this.updateStatus(InstanceStatus.Ok);
+
+		this.updateActions(); // export actions
+		this.updateVariableDefinitions(); // export variable definitions
 	}
+
 	// When module gets deleted
 	async destroy() {
 		this.log('debug', 'destroy')
@@ -29,12 +36,28 @@ class ModuleInstance extends InstanceBase {
 	// Return config fields for web config
 	getConfigFields() {
         var obj = [
+           {
+				id: 'defaultbrightness',
+				type: 'number',
+				label: 'Default Brightness',
+                width: 12,
+                'default': 100,
+                min: 1,
+                max: 100
+			},
+            {
+				id: 'defaultcolor',
+				type: 'textinput',
+				label: 'Default color (format: ff0000)',
+                width: 12,
+                'default': "ff0000"
+			},
             {
                 type: 'static-text',
                 id: 'info',
                 width: 12,
                 label: 'Information',
-                value: "Provide 1 to 5 Tally m5atom-matrix node. Default port is 6969"
+                value: "Provide 3 to 5 Tally m5atom-matrix node."
             }
         ];
     
@@ -45,14 +68,14 @@ class ModuleInstance extends InstanceBase {
 				label: 'Tally IP '+i,
 				width: 8
 			});
-            obj.push(
-			{
+            /*
+            obj.push({
 				type: 'textinput',
 				id: 'tallyport'+i,
 				label: 'Tally Port '+i,
 				width: 4,
                 'default': 6969
-			});
+			});*/
         }
 
 	    return obj;
@@ -67,21 +90,117 @@ class ModuleInstance extends InstanceBase {
                         type: 'dropdown',
                         label: 'State for tally '+i,
                         'default': 'UP',
-                        choices: [ {id: 'UP', label: 'UP'},{id: 'DOWN', label: 'DOWN'}]
+                        choices: [ {id: 'UP', label: 'UP'},{id: 'DOWN', label: 'DOWN'},{id: 'DONOTTOUCH', label: 'DO NOT CHANGE' }]
                 }
             );
         }
 
         this.setActionDefinitions({
             tally_update_action: {
-                name: 'Update Tally State',
+                name: 'Update tally state',
                 options: array_options,
-                callback: async (event) => {
-                    console.log('Hello world!', event.options)
+                callback: async (action, context) => {
+                    this.sendTallyCommand(action);
                 },
             },
+            tally_change_brightness: {
+                name: 'Change brightness for all', 
+                options: [{
+                    id: 'brightness',
+                    type: 'number',
+                    label: 'Brightness',
+                    'default': 100
+                }],
+                callback: async (action, context) => {
+                    if (this.current_brightness == null){
+                        this.current_brightness = action.options['brightness'];
+                    }
+                    console.log("new brightness " + action.options['brightness'] );
+                    this.sendTallyBrightnessChange( this.current_brightness );
+               },
+            },
+            tally_change_color: {
+                name: 'Change color for all',
+                options: [{
+                    id: 'color',
+                    type: 'textinput',
+                    label: 'Color'
+                }],
+                callback: async (action, context) => {
+                    console.log("new color "+ action.options['color'] );
+                
+                }
+            }
         });
 	}
+
+    async sendTallyBrightnessChange(brightness) {
+        for(var i = 1; i <= MAX_CAM; i++){
+            if (this.config['tallyip'+i] != "" && typeof(this.config['tallyip'+i]) != "undefined"){
+                try {
+                    // TODO OVERIDE DEFAULT BRIGHTNESS
+                    var qs = querystring.stringify({
+                        brightness: brightness
+                    });
+                    const options = {
+                        hostname: this.config['tallyip'+i],
+                        port: 80,
+                        path: '/?'+qs,
+                        method: 'GET', 
+                    };
+                    console.log(options);
+                    const req = http.get(options, (res) => {
+                        console.log('statusCode:', res.statusCode);
+                    });
+
+                    this.updateStatus(InstanceStatus.Ok)                     
+                } catch (e) {
+                    this.log('error', `http post request failed (${e.message})`)
+                    this.updatestatus(instancestatus.unknownerror, e.code)
+                } 
+            }
+        }
+        // end tally loop
+    }
+
+
+    async sendTallyCommand(action) {
+        for(var i = 1; i <= MAX_CAM; i++){
+            if (this.config['tallyip'+i] != "" && typeof(this.config['tallyip'+i]) != "undefined"){
+                var url = "http://"+this.config['tallyip'+i]+"/";
+                try {
+                    var new_state = action.options['cam'+i];
+                    if (new_state == "DONOTTOUCH"){
+                        continue;
+                    }
+                    var qs = querystring.stringify({
+                        state: new_state, 
+                        brightness: this.current_brightness // always overide brightness, to use default one
+                    });
+                    const options = {
+                        hostname: this.config['tallyip'+i],
+                        port: 80,
+                        path: '/?'+qs,
+                        method: 'GET', 
+                    };
+                    console.log(options);
+                    const req = http.get(options, (res) => {
+                        console.log('statusCode:', res.statusCode);
+                        /*console.log('headers:', res.headers);
+                      
+                        res.on('data', (d) => {
+                            console.log(d);
+                        }); */
+                    });
+
+                    this.updateStatus(InstanceStatus.Ok)
+                } catch (e) {
+                    this.log('error', `http post request failed (${e.message})`)
+                    this.updatestatus(instancestatus.unknownerror, e.code)
+                } 
+            }
+        }
+    }
 
 	updateFeedbacks() {
 		//UpdateFeedbacks(this)
